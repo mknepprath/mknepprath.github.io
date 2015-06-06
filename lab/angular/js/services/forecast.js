0bc247739e44f6106938b793934856906f17d88e
Michael Knepprath <mknepprath@gmail.com>
2015-06-06 18:54:51 -0500

    updated to forecast.json

--- lab/angular/js/services/forecast.js
+++ lab/angular/js/services/forecast.js
@@ -1,5 +1,5 @@
 app.factory('forecast', ['$http', function($http) {
-  return $http.get('http://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+  return $http.get('http://www.mknepprath.com/lab/angular/forecast.json')
             .success(function(data) {
               return data;
             })
