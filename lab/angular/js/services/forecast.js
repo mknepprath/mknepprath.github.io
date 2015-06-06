95a66b3ef75acdd6a349a1be478a41fbae791cd9
Michael Knepprath <mknepprath@gmail.com>
2015-06-05 19:09:12 -0500

    updated link to json file

--- lab/angular/js/services/forecast.js
+++ lab/angular/js/services/forecast.js
@@ -1,5 +1,5 @@
 app.factory('forecast', ['$http', function($http) {
-  return $http.get('products.json')
+  return $http.get('http://www.mknepprath.com/lab/angular/products.json')
             .success(function(data) {
               return data;
             })
