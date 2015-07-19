e08bc966cb2e620cec30867697b19e8959f192e1
Michael Knepprath <mknepprath@gmail.com>
2015-07-19 09:09:18 -0500

    added assets and updated ja site

--- lab/justactions/dist/js/main.js
+++ lab/justactions/dist/js/main.js
@@ -5002,8 +5002,18 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-$('.slick-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+$('.slider-for').slick({
+ infinite: true,
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ arrows: true,
+ asNavFor: '.slider-nav',
+ centerMode: true,
+ variableWidth: true
+});
+$('.slider-nav').slick({
+ slidesToShow: 10,
+ asNavFor: '.slider-for',
+ variableWidth: true,
+ focusOnSelect: true
 });
--- lab/justactions/js/main.js
+++ lab/justactions/js/main.js
@@ -1,5 +1,15 @@
-$('.slick-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+$('.slider-for').slick({
+ infinite: true,
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ arrows: true,
+ asNavFor: '.slider-nav',
+ centerMode: true,
+ variableWidth: true
+});
+$('.slider-nav').slick({
+ slidesToShow: 10,
+ asNavFor: '.slider-for',
+ variableWidth: true,
+ focusOnSelect: true
 });
