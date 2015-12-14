7a2e980658713fcd12580fd383cda4541ffe2392
Michael Knepprath <mknepprath@gmail.com>
2015-12-14 09:42:33 -0600

    adding comments, adding rows, moving styles into stylesheet, more options

--- lab/lilt/js/game.js
+++ lab/lilt/js/game.js
@@ -1,3 +1,4 @@
+// init cookie on first visit
 if (!(Cookies.get('position'))) {
   Cookies.set('position', 'start');
   $( ".command" ).prepend( "<li class='list-group-item list-group-item-warning'>&quot;Start&quot; to play.</li>" );
@@ -7,11 +8,15 @@ else {
 };
 var response = "";
 
+// when Tweet button is clicked...
 $( "#tweet" ).click(function() {
 
+  // grab tweet
   var tweet = $('#move').val();
-  var move = $('#move').val().toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  // sanitize tweet
+  var move = tweet.toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
+  // start
   if (Cookies.get('position') === "start") {
 
     if (move === "start") {
@@ -24,13 +29,15 @@ $( "#tweet" ).click(function() {
 
   }
 
+  // cell
   else if (Cookies.get('position') === "cell") {
 
-    if (move === "look at room") {
-      response = "Typical room, four walls, one is made of bars."
+    // look options
+    if (move === "look at back wall") {
+      response = "Your comfy bed is there, with pillow and blanket. A bowl of old(ish) apple paste sits next to it."
     }
-    else if (move === "look at walls") {
-      response = "There’s a front wall with bars, back, left, and right wall. That front wall looks pretty interesting."
+    else if (move === "look at blue bird" || move === "look at bird" || move === "look at the bird") {
+      response = "It’s song gives you hope."
     }
     else if (move === "look at front wall") {
       response = "There’s a door, and through the bars you see a trail of large ants and a key on the floor out of reach."
@@ -38,15 +45,24 @@ $( "#tweet" ).click(function() {
     else if (move === "look at right wall") {
       response = "There’s a window with a floor on the sill."
     }
+    else if (move === "look at room" || move === "look around") {
+      response = "Typical room, four walls, one is made of bars."
+    }
+    else if (move === "look at walls") {
+      response = "There’s a front wall with bars, back, left, and right wall. That front wall looks pretty interesting."
+    }
     else if (move === "look at window" || move === "look through window" || move === "look out window") {
       response = "There's a dense forest. You can see a blue bird tweeting."
     }
+    // open options
     else if (move === "open door" || move === "open the door") {
       response = "Surprise, no can do."
     }
+    // pick up options
     else if (move === "pick up ants" || move === "pick up ant" || move === "pick up an ant") {
       response = "They bite you. You drop it."
     }
+    // go through options
     else if (move === "go through window") {
       response = "You are much larger than the window."
     }
@@ -60,12 +76,16 @@ $( "#tweet" ).click(function() {
     response = "You should not be here."
   };
 
+  // logs tweet
   $( ".command" ).prepend( "<li class='list-group-item' data-position='" + Cookies.get('position') + "'>@mknepprath " + tweet + "</li>" );
+  // logs response
   $( ".command" ).prepend( "<li class='list-group-item list-group-item-info'>@familiarlilt " + response + "</li>" );
+  // clears tweet
   $('#move').val('');
 
 });
 
+// reset button, deletes cookie & refreshes page
 $( "#reset" ).click(function() {
   Cookies.remove('position');
   location.reload();
