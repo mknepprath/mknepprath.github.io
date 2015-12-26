f797f93e3951cf76c8075a24cf227b86904f9ce2
Michael Knepprath <mknepprath@gmail.com>
2015-12-26 13:00:53 -0600

    moved logs tweet/stores tweet/analytics above moves

--- lab/lilt/js/game.js
+++ lab/lilt/js/game.js
@@ -48,6 +48,19 @@ $( "#tweet" ).click(function() {
   // sanitize tweet
   var move = tweet.toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
+  // logs tweet
+  reply('', '@user ' + tweet)
+  // clears tweet
+  $('#move').val('');
+
+  // stores tweet
+  var dimensions = {
+    // Define ranges to bucket data points into meaningful segments
+    tweet: tweet,
+  };
+  // Send the dimensions to Parse along with the 'search' event
+  Parse.Analytics.track('tweets', dimensions);
+
   // init moves
   var moves = [];
   // init replied
@@ -63,14 +76,13 @@ $( "#tweet" ).click(function() {
         // assign current move to m
         m = ms[i];
         // if user entry matches move AND current position matches required position AND condition is true (like playinglilt or chestopen), continue - otherwise try next move
-        if (move === m.get('move') && position === m.get('position') && interactions[m.get('condition')].status === true) {
+        if (move === m.get('move') && position === m.get('position') && interactions[m.get('condition')].status === true) { // THIS NEEDS FIXING: if a move has two possible conditions (playinglilt/chestopen), game will reply both - maybe need an off state for objects? like chestclosed. or add "priority" row - display response with highest priority
+          console.log(move + ", " + position + ", " + interactions[m.get('condition')].status);
           // reply and set replied to true
           reply('info', '@familiarlilt ' + m.get('response'));
           replied = true;
           // if this move triggers an interaction, trigger it
-          if (m.get('trigger') !== undefined) {
-            interactions[m.get('trigger')].status = true;
-          }
+          interactions[m.get('trigger')].status = true;
           // if they enter 'start' at start, change position - janky right now, probably should become a column in Moves similar to trigger
           if (move === "start" && position === "start") {
             position = "cell";
@@ -82,30 +94,16 @@ $( "#tweet" ).click(function() {
         var response_options = ["You can't do that.", "That can't be done.", "Didn't work.", "Oops, can't do that.", "Sorry, you can't do that.", "That didn't work.", "Try something else.", "Sorry, you'll have to try something else.", "Oops, didn't work.", "Oops, try something else.", "Nice try, but you can't do that.", "Nice try, but that didn't work."];
         reply('info', '@familiarlilt ' + response_options[(Math.floor(Math.random() * response_options.length))]);
       }
+      // update cookies
+      for (var i in interactions) {
+        Cookies.set(interactions[i].action, interactions[i].status)
+      }
+      Cookies.set('position', position);
     },
     error: function(error) {
       console.log("Error: " + error.code + " " + error.message);
     }
-  });
-
-  // stores tweet
-  var dimensions = {
-    // Define ranges to bucket data points into meaningful segments
-    tweet: tweet,
-  };
-  // Send the dimensions to Parse along with the 'search' event
-  Parse.Analytics.track('tweets', dimensions);
-
-  // logs tweet
-  reply('', '@user ' + tweet)
-  // clears tweet
-  $('#move').val('');
-
-  // update cookies
-  for (var i in interactions) {
-    Cookies.set(interactions[i].action, interactions[i].status)
-  }
-  Cookies.set('position', position);
+  })
 });
 
 // runs click function when "enter" key is pressed
