6d3cb23de9891a4775251bf6a33802ed994f7635
Michael Knepprath <mknepprath@gmail.com>
2015-12-27 13:20:34 -0600

    moved Parse data grab out of #tweet so it gets called once rather than each move

--- lab/lilt/js/game.js
+++ lab/lilt/js/game.js
@@ -1,12 +1,9 @@
 // PARSE
 Parse.initialize("Beyi6l08p3QphWjpryXqnF66UN41wU7SFBwhFvQX", "FZmRAeAj3PRXFWiTY4C5KEWBwQgRLFchWX4uhACZ");
-// get data from Parse
+
+// get data from Parse for interactions
 var Interactions = Parse.Object.extend("Interactions");
 var intsquery = new Parse.Query(Interactions).limit(1000);
-var Moves = Parse.Object.extend("Moves");
-var movesquery = new Parse.Query(Moves).limit(1000);
-
-// sets up interactions
 var interactions = [];
 intsquery.find({
   success: function(ints) {
@@ -26,6 +23,20 @@ intsquery.find({
     console.log("Error: " + error.code + " " + error.message);
   }
 });
+
+// get data from Parse for moves
+var Moves = Parse.Object.extend("Moves");
+var movesquery = new Parse.Query(Moves).limit(1000);
+var moves = [];
+movesquery.find({
+  success: function(ms) {
+    console.log("Successfully retrieved " + ms.length + " moves.");
+    moves = ms;
+  },
+  error: function(error) {
+    console.log("Error: " + error.code + " " + error.message);
+  }
+})
 
 // if no cookie, create it and set position, else get the position
 if (!(Cookies.get('position'))) {
@@ -52,7 +63,6 @@ $( "#tweet" ).click(function() {
   var tweet = $('#move').val();
   // sanitize tweet
   var move = tweet.toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-
   // logs tweet
   reply('', '@user ' + tweet)
   // clears tweet
@@ -66,59 +76,48 @@ $( "#tweet" ).click(function() {
   // Send the dimensions to Parse along with the 'search' event
   Parse.Analytics.track('tweets', dimensions);
 
-  // init moves
-  var moves = [];
-  // get moves from Parse
-  movesquery.find({
-    success: function(ms) {
-      console.log("Successfully retrieved " + ms.length + " moves.");
-      var condition = 0;
-      var response = "";
-      // loop through moves
-      for (var i in ms) {
-        // assign current move to m
-        m = ms[i];
-        // if user entry matches move AND current position matches required position AND condition is true (like playinglilt or chestopen), get all info for move with highest condition. not sure if this makes sense, but it works so far
-        if (move === m.get('move') && position === m.get('position') && interactions[m.get('condition')].status === true) {
-          if (m.get('condition') >= condition) {
-            condition = m.get('condition');
-            response = m.get('response');
-            var trigger = m.get('trigger');
-            var halt = m.get('halt')
-          }
-        }
+  var condition = 0;
+  var response = "";
+  // loop through moves
+  for (var i in moves) {
+    // assign current move to m
+    m = moves[i];
+    // if user entry matches move AND current position matches required position AND condition is true (like playinglilt or chestopen), get all info for move with highest condition. not sure if this makes sense, but it works so far
+    if (move === m.get('move') && position === m.get('position') && interactions[m.get('condition')].status === true) {
+      if (m.get('condition') >= condition) {
+        condition = m.get('condition');
+        response = m.get('response');
+        var trigger = m.get('trigger');
+        var halt = m.get('halt')
       }
-      // if there is a response for their move, reply
-      if (response !== "") {
-        reply('info', '@familiarlilt ' + response);
-        // if this move triggers an interaction, trigger it
-        if (trigger !== undefined) {
-          interactions[trigger].status = true;
-        }
-        // if this move halts an interaction, halt it
-        if (halt !== undefined) {
-          interactions[halt].status = false;
-        }
-        // if they enter 'start' at start, change position - janky right now, probably should become a column in Moves similar to trigger
-        if (move === "start" && position === "start") {
-          position = "cell";
-        }
-      }
-      // if no move was valid, reply with one of these error messages
-      else {
-        var response_options = ["You can't do that.", "That can't be done.", "Didn't work.", "Oops, can't do that.", "Sorry, you can't do that.", "That didn't work.", "Try something else.", "Sorry, you'll have to try something else.", "Oops, didn't work.", "Oops, try something else.", "Nice try, but you can't do that.", "Nice try, but that didn't work."];
-        reply('info', '@familiarlilt ' + response_options[(Math.floor(Math.random() * response_options.length))]);
-      }
-      // update cookies
-      for (var i in interactions) {
-        Cookies.set(interactions[i].action, interactions[i].status)
-      }
-      Cookies.set('position', position);
-    },
-    error: function(error) {
-      console.log("Error: " + error.code + " " + error.message);
     }
-  })
+  }
+  // if there is a response for their move, reply
+  if (response !== "") {
+    reply('info', '@familiarlilt ' + response);
+    // if this move triggers an interaction, trigger it
+    if (trigger !== undefined) {
+      interactions[trigger].status = true;
+    }
+    // if this move halts an interaction, halt it
+    if (halt !== undefined) {
+      interactions[halt].status = false;
+    }
+    // if they enter 'start' at start, change position - janky right now, probably should become a column in Moves similar to trigger
+    if (move === "start" && position === "start") {
+      position = "cell";
+    }
+  }
+  // if no move was valid, reply with one of these error messages
+  else {
+    var response_options = ["You can't do that.", "That can't be done.", "Didn't work.", "Oops, can't do that.", "Sorry, you can't do that.", "That didn't work.", "Try something else.", "Sorry, you'll have to try something else.", "Oops, didn't work.", "Oops, try something else.", "Nice try, but you can't do that.", "Nice try, but that didn't work."];
+    reply('info', '@familiarlilt ' + response_options[(Math.floor(Math.random() * response_options.length))]);
+  }
+  // update cookies
+  for (var i in interactions) {
+    Cookies.set(interactions[i].action, interactions[i].status)
+  }
+  Cookies.set('position', position);
 });
 
 // runs click function when "enter" key is pressed
